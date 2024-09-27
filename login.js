688da7c85283591350f2216e14f8d9f758cdb5fb
Zedliano <enmanuela72@gmail.com>
2024-09-26 21:26:40 -0600

    commit

---
+++ login.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+
+const btnSingIn = document.getElementById("btn-sing-in")
+const btnSingUp = document.getElementById("btn-sing-up")
+
+btnSingIn.addEventListener("click",()=>{
+    container.classList.remove("toggle");
+});
+btnSingUp.addEventListener("click",()=>{
+container.classList.add("toggle")
+});
